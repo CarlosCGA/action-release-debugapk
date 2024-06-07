661ab6d48aeb62ccf5e58373bb1b11eecec4faf3
Carlos <carloscga97@gmail.com>
2024-06-07 13:11:29 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,5 @@
 
 sh -c "git config --global --add safe.directory $PWD"
 
-#hub checkout master
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
