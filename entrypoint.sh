dc444b7ea80485e56c6e0d953534b6ead575efa7
Carlos <carloscga97@gmail.com>
2024-06-15 18:23:55 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ sh -c "git config --global --add safe.directory $PWD"
 
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
 #VERSION_NAME=`grep -oP 'versionName.txt' ./${APP_FOLDER}/build.gradle.kts`
+
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
 #hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/My Login_3.0.3.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
