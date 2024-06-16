cbdb75eb16d2671d3feb1e408ec28f975cb0646f
Carlos <carloscga97@gmail.com>
2024-06-16 22:30:28 +0200

    Update entrypoint.sh
    Remove RELEASE_TITLE before version

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ sh -c "git config --global --add safe.directory $PWD"
 VERSION_NAME=`cat ./${APP_FOLDER}/versionName.txt`
 #VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/versionName.txt`
 
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*.apk -m "v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
+#hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
 #hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
