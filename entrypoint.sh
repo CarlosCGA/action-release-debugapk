8ce3468f60c29ec019377440913e722717444591
Carlos <carloscga97@gmail.com>
2024-06-16 12:37:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 sh -c "git config --global --add safe.directory $PWD"
 
-VERSION_NAME=`grep ./${APP_FOLDER}/versionName.txt`
+#VERSION_NAME=`grep ./${APP_FOLDER}/versionName.txt`
 #VERSION_NAME=`grep 'soy' ./${APP_FOLDER}/versionName.txt`
 #VERSION_NAME=`grep -oP 'soy' ./${APP_FOLDER}/versionName.txt`
 #VERSION_NAME=`grep -oP 'soy "\K(.*?)(?=")' ./${APP_FOLDER}/versionName.txt`
-#VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
+VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
 #VERSION_NAME=`grep -oP 'versionName.txt' ./${APP_FOLDER}/build.gradle.kts`
 
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
