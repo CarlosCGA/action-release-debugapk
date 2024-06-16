072032a9113e941e3aa5cf292b81f3b050c77904
Carlos <carloscga97@gmail.com>
2024-06-16 12:16:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 sh -c "git config --global --add safe.directory $PWD"
 
-VERSION_NAME=`grep 'soy' ./${APP_FOLDER}/versionName.txt`
+VERSION_NAME=`grep ./${APP_FOLDER}/versionName.txt`
+#VERSION_NAME=`grep 'soy' ./${APP_FOLDER}/versionName.txt`
 #VERSION_NAME=`grep -oP 'soy' ./${APP_FOLDER}/versionName.txt`
 #VERSION_NAME=`grep -oP 'soy "\K(.*?)(?=")' ./${APP_FOLDER}/versionName.txt`
 #VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
