f5e06e0adf94e893be1aae5bf5d59b3994f1595b
Carlos <carloscga97@gmail.com>
2024-06-16 12:23:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ VERSION_NAME=`grep ./${APP_FOLDER}/versionName.txt`
 #VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle.kts`
 #VERSION_NAME=`grep -oP 'versionName.txt' ./${APP_FOLDER}/build.gradle.kts`
 
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
 #hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
